c62dfb8cd835c8f8f6465cb6cc8aa0d361970de2
Nikolay Danailov <frostblooded@yahoo.com>
2025-07-13 11:52:01 +0300

    Make meteorites collide with player and planet

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -5,8 +5,12 @@ extends Node
 @export var _initial_player_radius: float = 100.0
 @export var _default_rotation_speed: float = 1.0
 @export var _increased_rotation_speed: float = 2.0
+@export var _area: Area2D
 
 var _player_angle: float = 0.0
+
+func _enter_tree() -> void:
+    _area.area_entered.connect(_on_hit)
 
 func _ready() -> void:
     _player.position = Vector2(0, _initial_player_radius)
@@ -23,3 +27,8 @@ func _set_player_position(delta: float) -> void:
 
     _player_angle += used_rotation_speed * delta
     _player.position = Globals.get_position_on_circle(Vector2.ZERO, _initial_player_radius, _player_angle)
+
+func _on_hit(area: Area2D) -> void:
+    if area.owner.is_in_group("meteorites"):
+        print("Hit by meteorite!")
+        area.owner.queue_free()
---
+++ scripts/player.gd
@@ -0,0 +1,12 @@
+class_name Player
+extends Node2D
+
+@export var _area: Area2D
+
+func _enter_tree() -> void:
+    _area.area_entered.connect(_on_hit)
+
+func _on_hit(area: Area2D) -> void:
+    if area.owner.is_in_group("meteorites"):
+        print("Player hit by meteorite!")
+        area.owner.queue_free()
